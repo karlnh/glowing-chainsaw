1c9e3b94c960a5f5dad24051ab29846eee524c0e
karlnh <hubigkarl@gmail.com>
2023-01-17 21:50:15 -0600

    clean server

--- server.js
+++ server.js
@@ -2,24 +2,25 @@ const express = require('express');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+const app = require('./routes/index');
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Middleware for serving static files
-app.use(express.static('public'));
 
-// GET route for default page
-app.get('*', (req, res) => {
-    console.info(`${req.method} request received for *`);
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+// Middleware for serving static files
+app.use(express.static('public')); // Everything in here is accessible from the URL
 
 // GET route for notes page
 app.get('/notes', (req, res) => {
     console.info(`${req.method} request received for /notes`);
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// GET route for default/home page
+app.get('*', (req, res) => {
+    console.info(`${req.method} request received for *`);
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => 
