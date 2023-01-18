de307feff499a4287e37fc746288fd8305f60c0a
karlnh <hubigkarl@gmail.com>
2023-01-17 23:08:58 -0600

    add middleware and new routers

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const path = require('path');
+const { logger } = require('./middleware/logger');
 
 const PORT = process.env.PORT || 3001;
-const app = require('./routes/index');
+const app = express();
+
+app.use(logger);
+
+const htmlRouter = require('./routes/htmlRoutes');
+
+const apiRouter = require('./routes/apiRoutes');
+// TO DO: ADD JSON OBJECT STUFF TO APIROUTES
+// TO DO: ADD JSON OBJECT STUFF TO APIROUTES
+// TO DO: ADD JSON OBJECT STUFF TO APIROUTES
 
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json());
@@ -11,17 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware for serving static files
 app.use(express.static('public')); // Everything in here is accessible from the URL
 
-// GET route for notes page
-app.get('/notes', (req, res) => {
-    console.info(`${req.method} request received for /notes`);
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
-// GET route for default/home page
-app.get('*', (req, res) => {
-    console.info(`${req.method} request received for *`);
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
 
 app.listen(PORT, () => 
 console.log(`Listening at http://localhost:${PORT}`)
