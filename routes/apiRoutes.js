c250beac6db0a1410ee1826801712068ee15d8ec
karlnh <hubigkarl@gmail.com>
2023-01-19 21:54:57 -0600

    add console info for requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,14 +5,14 @@ const router = require('express').Router(); // setting notes as router
 
 // GET for /api/notes used to retrieve db.json and return all saved notes as JSON
 router.get('/api/notes', (req, res) => {
-    console.info(`GET for ${req.path} successful!`)
+    console.info(`${req.method} for ${req.path} received.`)
     res.json(db);
 });
 
 // POST /api/notes should receive a new note to save in the request body and add it to db.json, then return new note to the client, as well as giving each note a unique ID when it's saved
 
 router.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received for /api/notes to add a note`);
+    console.info(`${req.method} for ${req.path} received.`);
 
     const { title, text } = req.body;
 
@@ -21,9 +21,14 @@ router.post('/api/notes', (req, res) => {
         text,
         id: uuidv4(),
     };
-    console.log(`New note: ${newNote}`);
+
+    console.info(
+    `New note received.
+    Contents: ${newNote}`
+    );
 
     fs.readFile('../db/db.json', "utf8", (err, data) => {
+        console.info('Pushing new note...');
         const dbJSON = JSON.parse(data);
         dbJSON.push(newNote);
         console.info('Pushed new note to parsed data.');
